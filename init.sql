b45179a42bb5e8930fe145ce0f2f8408dfada19c
mr.ritik99@gmail.com <mr.ritik99@gmail.com>
2024-05-29 16:27:14 +0530

    init.sql fixed

--- init.sql
+++ init.sql
@@ -4,9 +4,16 @@ GRANT ALL PRIVILEGES ON DATABASE dynamicJobs TO ritik;
 
 \connect dynamicJobs;
 
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    category TEXT NOT NULL,
-    price FLOAT NOT NULL
+CREATE SEQUENCE product_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999
+	START 1
+
+CREATE TABLE public.products (
+	id int4 NOT NULL DEFAULT nextval('product_id_seq'),
+	"name" varchar NULL,
+	category varchar NULL,
+	price float8 NULL,
+	CONSTRAINT products_pkey PRIMARY KEY (id)
 );
